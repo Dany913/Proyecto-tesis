6987ba0cde67a6163e8a771d7672faac16178d2e
Dany <lmayorgaa@unbosque.edu.co>
2025-12-09 17:04:26 -0500

    demo visual

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="card">
+          <h2>Cargar</h2>
+          <p>Carga el registro</p>
+        </div>
+        <div className="card">
+          <h2>Descargar</h2>
+          <p>Descargar Registro</p>
+        </div>
+        <div className="card">
+          <h2>Lista</h2>
+          <p>Vizualizacion de Registros</p>
+        </div>
+      </div>
     </div>
   );
 }
